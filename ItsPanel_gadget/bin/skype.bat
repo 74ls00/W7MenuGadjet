e8c18bd33622979c33a1b841af63e45e5cffbf8b
74ls00 <userled@yandex.ru>
2018-09-30 05:30:05 +0300

    .

--- ItsPanel_gadget/bin/skype.bat
+++ ItsPanel_gadget/bin/skype.bat
@@ -1,5 +1,10 @@
 
 
-start G:\Programs\portableapps.com\PortableApps\sPortable skype.bat
+
+
+start /d G:\Programs\portableapps.com\PortableApps\sPortable sPortable.exe /nosplash /minimized
+
+
+rem start G:\Programs\portableapps.com\PortableApps\sPortable skype.bat
 
 rem start /d G:\Programs\portableapps.com\PortableApps\SkypePortable skype.bat
