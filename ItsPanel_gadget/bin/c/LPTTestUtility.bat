f1b70daa3848910a318844d4d9b829cbb25d707e
74ls00 <userled@yandex.ru>
2018-11-12 01:52:24 +0300

    .

--- ItsPanel_gadget/bin/c/LPTTestUtility.bat
+++ ItsPanel_gadget/bin/c/LPTTestUtility.bat
@@ -1,3 +1,4 @@
 @echo off
 title LPTTestUtility
+rem start /d "C:\Program Files (x86)\X-Lent Electronics\LPTTestUtility" LPTTestUtility.exe
 start /d "C:\Program Files (x86)\X-Lent Electronics\LPTTestUtility" LPTTestUtility.exe
