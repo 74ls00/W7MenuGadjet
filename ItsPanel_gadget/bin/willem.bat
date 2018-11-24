bd2e1cfcff549621b1b7575562a06f9a7fff016a
74ls00 <userled@yandex.ru>
2018-11-24 21:28:48 +0300

    .

--- ItsPanel_gadget/bin/willem.bat
+++ ItsPanel_gadget/bin/willem.bat
@@ -1,4 +1,5 @@
 @echo off
 title EpromM51_98D12C3
 rem start /d "G:\home\Documents\Projects\0-Electronics\Programmer\EzoFlash4v5\soft\Willem 5F x64" EpromM51_98D12C3.exe
-start /d "G:\home\Documents\Projects\0-Electronics\Programmers\EzoFlash4v5\soft\Willem 5F x64" EpromM51_98D12C3.exe
+rem start /d "G:\home\Documents\Projects\0-Electronics\Programmers\EzoFlash4v5\soft\Willem 5F x64" EpromM51_98D12C3.exe
+start /d "G:\home\Documents\Projects\0-Electronics\Programmers\EzoFlash4v5\soft\Willem64" Willem64.bat
