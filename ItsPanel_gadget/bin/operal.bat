bc4f463589d59e5583263aebf3560aad6ac40476
74ls00 <userled@yandex.ru>
2018-12-01 07:35:40 +0300

    .

--- ItsPanel_gadget/bin/operal.bat
+++ ItsPanel_gadget/bin/operal.bat
@@ -1,7 +1,11 @@
 @echo off
+title Opera
 rem start /d G:\Programs\Programs\Opera\OperaPortable OperaPortable.exe
 
 rem cd /d G:\Programs\ProgramsW\Opera\Opera10
 rem start /abovenormal G:\Programs\ProgramsW\Opera\Opera10\opera.exe
 cls
-start /d G:\Programs\ProgramsW\Opera\Opera11 opera.exe
+rem start /d G:\Programs\ProgramsW\Opera\Opera11 opera.exe
+
+start /d G:\Programs\ProgramsW\Opera\OperaX64 Opera64.exe
+
