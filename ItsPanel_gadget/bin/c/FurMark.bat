bae006e0b469675e9e452ff61f519fe120fa908b
74ls00 <userled@yandex.ru>
2021-10-07 01:34:25 +0300

    fm

---
+++ ItsPanel_gadget/bin/c/FurMark.bat
@@ -0,0 +1,3 @@
+@echo off
+title FurMark
+start /d "C:\Program Files (x86)\Geeks3D\Benchmarks\FurMark" FurMark.exe
---
+++ ItsPanel_gadget/bin/c/gpuz.bat
@@ -0,0 +1,3 @@
+@echo off
+title GPU-Z
+start /d "C:\Program Files (x86)\Geeks3D\Benchmarks\FurMark" gpuz.exe
