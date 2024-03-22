98fba6d3695aa4df2da9a01e4dba8218873c9402
74ls00 <userled@yandex.ru>
2024-03-22 09:10:26 +0300

    .

--- ItsPanel_gadget/bin/freemind.bat
+++ ItsPanel_gadget/bin/freemind.bat
@@ -1,2 +1,3 @@
 rem start /d G:\Programs\ProgramsW\FreeMind Freemind.exe
+path=%path%;C:\Program Files\Java\jre1.8.0_201\bin;%path%
 start /d G:\Programs\portableapps.com\PortableApps\FreemindPortableBeta FreemindPortable.exe
