5314f35542f8884a6910f9704c1de4111d765f9f
74ls00 <userled@yandex.ru>
2024-05-29 19:25:24 +0300

    .

--- ItsPanel_gadget/bin/vivaldi.bat
+++ ItsPanel_gadget/bin/vivaldi.bat
@@ -6,4 +6,8 @@ rem for win 7
 rem start /d "D:\ProgramsW7\Vivaldi\Application" vivaldi.exe 
 
 rem for win 11
-start /d "C:\ProgramsW11\Vivaldi\Application" vivaldi.exe 
+rem start /d "C:\ProgramsW11\Vivaldi\Application" vivaldi.exe
+
+rem for win 11
+start /d "G:\Programs\Programs\Vivaldi\Application" vivaldi.exe
+
