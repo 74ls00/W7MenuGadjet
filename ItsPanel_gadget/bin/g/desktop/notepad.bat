63c30e423799a44e93705f63785fcb7eaef5af11
74ls00 <userled@yandex.ru>
2025-08-10 22:59:56 +0300

    wd

--- ItsPanel_gadget/bin/g/desktop/notepad.bat
+++ ItsPanel_gadget/bin/g/desktop/notepad.bat
@@ -2,3 +2,10 @@
 title notepad win xp
 start G:\Programs\Desktop\WindowsXP\XP_WINDOWS\NOTEPAD.EXE
 
+
+rem chcp 1251 | Echo "Блокнот был перезапущен">%tmp%\tmp.txt | timeout 5 |  G:\Programs\Desktop\WindowsXP\XP_WINDOWS\NOTEPAD.EXE %tmp%\tmp.txt | exit
+
+
+
+
+
---
+++ ItsPanel_gadget/bin/os/WDTS.bat
@@ -0,0 +1,1 @@
+explorer windowsdefender://ThreatSettings
